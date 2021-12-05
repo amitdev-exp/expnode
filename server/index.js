a345d4123928515c20e332fc064106a5cca39efa
Amit Vishwakarma <you@example.com>
2021-12-05 18:05:54 +0530

    Aded all logic

--- server/index.js
+++ server/index.js
@@ -1,14 +1,42 @@
 const express = require('express')
 const app  = express();
 const port = 5000;
+const fs = require("fs");
+const cors = require('cors');
+const req = require('http')
 
-app.get('/api/customers/', (req,res) => {
-    const customers = [
-        { id :1 , firstName: 'Amit' , lastName : 'Vishwakarma'},
-        { id :2 , firstName: 'Sumit' , lastName : 'Vishwakarma'},
-        { id :3 , firstName: 'Shweta' , lastName : 'Vishwakarma'},
-    ];
-    res.json(customers);
+app.use(cors())
+
+const loadData = function () {
+    try{
+        let databuffer = fs.readFileSync('./resources/sample.json')
+        let data = databuffer.toString();
+        return JSON.parse(data)
+
+    }catch(e){
+        console.log(e);
+    }
+}
+
+const saveData = function (data) {
+    const dataJSON = JSON.stringify(data);
+    fs.writeFileSync('./resources/sample.json',dataJSON)
+} 
+
+app.get('/api/readData/', (req,res) => {
+    const data = loadData();
+    res.send(data);
 });
+
+app.post('/api/writeData/', (req,res) => {
+    
+    console.log("data", req.query )
+    let data = loadData()
+    data.push(JSON.parse(req.query.value))
+    saveData(data)
+    res.send("Data saved successfully");
+});
+
+
 
 app.listen(port, () => console.log(`server is running on ${port}`));
